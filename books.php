f54ca6cc2ee83ec17372745f236cad650b626afb
indratamagiri <madegiri1212@gmail.com>
2017-12-30 00:10:42 +0700

    update search

--- books.php
+++ books.php
@@ -7,16 +7,26 @@ $db = "sql12213159";
 
 $connection = mysqli_connect($server, $username, $pass, $db);
 
-
-$query_result = $connection->query("SELECT * FROM book");
-
-$books = [];
-// looping semua data hasil query
-while ($data = $query_result->fetch_assoc()) {
-	$books[] = $data;
+if ($_REQUEST) {
+$search_query = $_REQUEST['q'];
+$query_result = $connection->query("SELECT * FROM book WHERE title LIKE '%$search_query%' ");
+}else{
+	$query_result = $connection->query("SELECT * FROM book");
 }
 
+
+$book = [];
+$count = 0;
+$books = new stdClass();
+// looping semua data hasil query
+while ($data = $query_result->fetch_assoc()) {
+	$book[] = $data;
+	$count=$count+1;
+}
+$books->count = $count;
+if ($_REQUEST) {
+$books->q = $search_query;
+}
+$books->book= $book;
 header('Content-Type: application/json');
 print json_encode($books);
-
-?>
